a38dd6d6be1972b91d93a6e1a7344284942b037a
AlessandroRG31 <danielalessandro1331@gmail.com>
2025-04-25 01:12:35 -0600

    Arreglar rutas y estilizar listado de vehículos con Bootstrap

--- mantenimiento/urls.py
+++ mantenimiento/urls.py
@@ -2,14 +2,12 @@
 from django.urls import path
 from . import views
 
+# mantenimiento/urls.py
 app_name = 'mantenimiento'
-
 urlpatterns = [
-    path('vehiculos/', views.listar_vehiculos,    name='vehiculo_list'),
+    path('vehiculos/', views.listar_vehiculos, name='vehiculo_list'),
     path('vehiculos/nuevo/', views.crear_vehiculo, name='vehiculo_nuevo'),
     path('mantenimientos/', views.listar_mantenimientos, name='mantenimiento_list'),
     path('mantenimientos/nuevo/', views.crear_mantenimiento, name='mantenimiento_nuevo'),
     path('mantenimientos/nuevo/<int:vehiculo_id>/', views.crear_mantenimiento, name='mantenimiento_nuevo_con_vehiculo'),
 ]
-
-
