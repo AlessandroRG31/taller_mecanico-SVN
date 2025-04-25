e71cd739c63cea2170490ddc745a5998547f31b6
AlessandroRG31 <danielalessandro1331@gmail.com>
2025-04-25 03:57:31 -0600

    CRUD Empresa y Repuesto: agregar forms, views y urls faltantes

--- repuestos/urls.py
+++ repuestos/urls.py
@@ -4,13 +4,16 @@ from . import views
 app_name = 'repuestos'
 
 urlpatterns = [
+    # Búsqueda pública
     path('', views.buscar_repuestos, name='buscar_repuestos'),
 
+    # CRUD Empresa
     path('empresas/', views.EmpresaListView.as_view(), name='empresa_list'),
     path('empresa/nuevo/', views.EmpresaCreateView.as_view(), name='empresa_nuevo'),
     path('empresa/<int:pk>/editar/', views.EmpresaUpdateView.as_view(), name='empresa_editar'),
     path('empresa/<int:pk>/eliminar/', views.EmpresaDeleteView.as_view(), name='empresa_eliminar'),
 
+    # CRUD Repuesto
     path('repuestos/', views.RepuestoListView.as_view(), name='repuesto_list'),
     path('repuesto/nuevo/', views.RepuestoCreateView.as_view(), name='repuesto_nuevo'),
     path('repuesto/<int:pk>/editar/', views.RepuestoUpdateView.as_view(), name='repuesto_editar'),
