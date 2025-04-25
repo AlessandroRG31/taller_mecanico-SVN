77f6b26c1693f223768e319f251d7c5075039447
AlessandroRG31 <danielalessandro1331@gmail.com>
2025-04-25 05:14:07 -0600

    Admin: quitar filter_horizontal y añadir inline RepuestoMantenimiento

--- mantenimiento/admin.py
+++ mantenimiento/admin.py
@@ -1,20 +1,28 @@
+# mantenimiento/admin.py
+
 from django.contrib import admin
-from .models import Vehiculo, ProximoMantenimiento, Mantenimiento
+from .models import Vehiculo, ProximoMantenimiento, Mantenimiento, RepuestoMantenimiento
 
 class ProximoMantenimientoInline(admin.TabularInline):
     model = ProximoMantenimiento
     extra = 2  # cuántas fechas permitir añadir por defecto
 
+class RepuestoMantenimientoInline(admin.TabularInline):
+    model = RepuestoMantenimiento
+    extra = 1  # al menos una línea para cantidad de repuesto
+
 @admin.register(Vehiculo)
 class VehiculoAdmin(admin.ModelAdmin):
-    list_display   = ['placa', 'cliente', 'marca', 'modelo', 'anio', 'tipo', 'costo']
-    list_filter    = ['tipo', 'anio']
-    search_fields  = ['placa', 'cliente', 'marca', 'modelo']
-    inlines        = [ProximoMantenimientoInline]
+    list_display = ['placa', 'cliente', 'marca', 'modelo', 'anio', 'tipo', 'costo']
+    list_filter = ['tipo', 'anio']
+    search_fields = ['placa', 'cliente', 'marca', 'modelo']
+    inlines = [ProximoMantenimientoInline]
 
 @admin.register(Mantenimiento)
 class MantenimientoAdmin(admin.ModelAdmin):
-    list_display      = ['vehiculo', 'tipo_mantenimiento', 'fecha_mantenimiento', 'costo']
-    list_filter       = ['tipo_mantenimiento', 'fecha_mantenimiento']
-    search_fields     = ['vehiculo__placa', 'tipo_mantenimiento']
-    filter_horizontal = ['repuestos']
+    list_display = ['vehiculo', 'tipo_mantenimiento', 'fecha_mantenimiento', 'costo']
+    list_filter = ['tipo_mantenimiento', 'fecha_mantenimiento']
+    search_fields = ['vehiculo__placa', 'tipo_mantenimiento']
+    # Quitamos filter_horizontal en ManyToMany con through
+    # filter_horizontal = ['repuestos']
+    inlines = [RepuestoMantenimientoInline]
