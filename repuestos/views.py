4df48e04a912585169611e38e0a7145a596c1e4f
AlessandroRG31 <danielalessandro1331@gmail.com>
2025-04-24 21:13:37 -0600

    Repuestos: añadir plantilla repuesto_list.html y alinear view

--- repuestos/views.py
+++ repuestos/views.py
@@ -1,29 +1,21 @@
-# repuestos/views.py
-
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from .models import Repuesto
 
 def buscar_repuestos(request):
-    # 1) Solo stock > 0
     qs = Repuesto.objects.select_related('empresa').filter(stock__gt=0)
-
-    # 2) Filtrado por nombre
     q = request.GET.get('q', '')
     if q:
         qs = qs.filter(nombre__icontains=q)
-
-    # 3) Orden por precio (ascendente por defecto)
     orden = request.GET.get('orden', 'precio')
     qs = qs.order_by(orden)
 
-    # 4) Paginación
-    paginator = Paginator(qs, 10)  # 10 repuestos por página
+    paginator = Paginator(qs, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
-    return render(request, 'repuestos/repuesto_search.html', {
-    'resultados': qs,
-    'query': query,
-})
-
+    return render(request, 'repuestos/repuesto_list.html', {
+        'page_obj': page_obj,
+        'q': q,
+        'orden': orden,
+    })
