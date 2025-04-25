57b419b99712ee04548e70dcd147dc637935df33
AlessandroRG31 <danielalessandro1331@gmail.com>
2025-04-25 03:42:49 -0600

    Repuestos: añadir forms para CRUD de Empresa y Repuesto

--- repuestos/forms.py
+++ repuestos/forms.py
@@ -1,7 +1,22 @@
 from django import forms
-from .models import Repuesto
+from .models import Empresa, Repuesto
+
+class EmpresaForm(forms.ModelForm):
+    class Meta:
+        model = Empresa
+        fields = ['nombre', 'telefono']
+        labels = {
+            'nombre': 'Nombre de la empresa',
+            'telefono': 'Teléfono',
+        }
 
 class RepuestoForm(forms.ModelForm):
     class Meta:
         model = Repuesto
         fields = ['empresa', 'nombre', 'precio', 'stock']
+        labels = {
+            'empresa': 'Empresa vendedora',
+            'nombre': 'Nombre del repuesto',
+            'precio': 'Precio (S/.)',
+            'stock': 'Stock disponible',
+        }
