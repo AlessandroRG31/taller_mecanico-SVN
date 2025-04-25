00126ce2bd9ead7d40d74ea49146b2912cae69dc
AlessandroRG31 <danielalessandro1331@gmail.com>
2025-04-25 03:53:34 -0600

    Agregar CRUD completo de Empresa y Repuesto

--- repuestos/urls.py
+++ repuestos/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-from .views      import buscar_repuestos
+from . import views
 
 app_name = 'repuestos'
 
 urlpatterns = [
-    path('', buscar_repuestos, name='buscar_repuestos'),
+    path('', views.buscar_repuestos, name='buscar_repuestos'),
+
+    path('empresas/', views.EmpresaListView.as_view(), name='empresa_list'),
+    path('empresa/nuevo/', views.EmpresaCreateView.as_view(), name='empresa_nuevo'),
+    path('empresa/<int:pk>/editar/', views.EmpresaUpdateView.as_view(), name='empresa_editar'),
+    path('empresa/<int:pk>/eliminar/', views.EmpresaDeleteView.as_view(), name='empresa_eliminar'),
+
+    path('repuestos/', views.RepuestoListView.as_view(), name='repuesto_list'),
+    path('repuesto/nuevo/', views.RepuestoCreateView.as_view(), name='repuesto_nuevo'),
+    path('repuesto/<int:pk>/editar/', views.RepuestoUpdateView.as_view(), name='repuesto_editar'),
+    path('repuesto/<int:pk>/eliminar/', views.RepuestoDeleteView.as_view(), name='repuesto_eliminar'),
 ]
--- repuestos/views.py
+++ repuestos/views.py
@@ -1,21 +1,57 @@
-from django.shortcuts     import render
-from django.core.paginator import Paginator
-from .models              import Repuesto
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
+from .models import Empresa, Repuesto
+from .forms import EmpresaForm, RepuestoForm
 
 def buscar_repuestos(request):
-    qs = Repuesto.objects.select_related('empresa').filter(stock__gt=0)
-    q  = request.GET.get('q', '')
-    if q:
-        qs = qs.filter(nombre__icontains=q)
     orden = request.GET.get('orden', 'precio')
-    qs    = qs.order_by(orden)
+    if orden not in ['precio', '-precio', 'stock', '-stock', 'nombre', '-nombre']:
+        orden = 'precio'
+    repuestos = Repuesto.objects.filter(stock__gt=0).order_by(orden)
+    return render(request, 'repuestos/repuesto_search.html', {'repuestos': repuestos})
 
-    paginator   = Paginator(qs, 10)
-    page_number = request.GET.get('page')
-    page_obj    = paginator.get_page(page_number)
+class EmpresaListView(ListView):
+    model = Empresa
+    template_name = 'repuestos/empresa_list.html'
+    context_object_name = 'empresas'
 
-    return render(request, 'repuestos/repuesto_search.html', {
-        'page_obj': page_obj,
-        'q'       : q,
-        'orden'   : orden,
-    })
+class EmpresaCreateView(CreateView):
+    model = Empresa
+    form_class = EmpresaForm
+    template_name = 'repuestos/empresa_form.html'
+    success_url = reverse_lazy('repuestos:empresa_list')
+
+class EmpresaUpdateView(UpdateView):
+    model = Empresa
+    form_class = EmpresaForm
+    template_name = 'repuestos/empresa_form.html'
+    success_url = reverse_lazy('repuestos:empresa_list')
+
+class EmpresaDeleteView(DeleteView):
+    model = Empresa
+    template_name = 'repuestos/empresa_confirm_delete.html'
+    success_url = reverse_lazy('repuestos:empresa_list')
+
+class RepuestoListView(ListView):
+    model = Repuesto
+    template_name = 'repuestos/repuesto_list.html'
+    context_object_name = 'repuestos'
+
+class RepuestoCreateView(CreateView):
+    model = Repuesto
+    form_class = RepuestoForm
+    template_name = 'repuestos/repuesto_form.html'
+    success_url = reverse_lazy('repuestos:repuesto_list')
+
+class RepuestoUpdateView(UpdateView):
+    model = Repuesto
+    form_class = RepuestoForm
+    template_name = 'repuestos/repuesto_form.html'
+    success_url = reverse_lazy('repuestos:repuesto_list')
+
+class RepuestoDeleteView(DeleteView):
+    model = Repuesto
+    template_name = 'repuestos/repuesto_confirm_delete.html'
+    success_url = reverse_lazy('repuestos:repuesto_list')
