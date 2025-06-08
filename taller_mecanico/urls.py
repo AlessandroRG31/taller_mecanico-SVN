e7be1b8ed3d06ec7a6756b892649928914cac2fc
AlessandroRG31 <danielalessandro1331@gmail.com>
2025-06-08 16:18:29 -0600

    feat: configuración global – añadir rutas autocomplete de django-autocomplete-light

--- taller_mecanico/urls.py
+++ taller_mecanico/urls.py
@@ -9,5 +9,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('core.urls', 'core'), namespace='core')),
     path('mantenimientos/', include(('mantenimiento.urls', 'mantenimiento'), namespace='mantenimiento')),
-    path('repuestos/',      include(('repuestos.urls', 'repuestos'),       namespace='repuestos')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('repuestos/', include(('repuestos.urls', 'repuestos'), namespace='repuestos')),
+
+    # django-autocomplete-light
+    path('autocomplete/', include('dal.urls')),
+    path('autocomplete/', include('dal_select2.urls')),
+]
+
+# Servir archivos multimedia en desarrollo
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
