0d103348a6ca24346ab75d10834f83569218cfa7
AlessandroRG31 <danielalessandro1331@gmail.com>
2025-06-08 16:39:56 -0600

    fix: remover rutas dal.urls obsoletas de urls.py

--- taller_mecanico/urls.py
+++ taller_mecanico/urls.py
@@ -1,5 +1,3 @@
-# ruta: taller_mecanico/taller_mecanico/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -10,11 +8,11 @@ urlpatterns = [
     path('', include(('core.urls', 'core'), namespace='core')),
     path('mantenimientos/', include(('mantenimiento.urls', 'mantenimiento'), namespace='mantenimiento')),
     path('repuestos/', include(('repuestos.urls', 'repuestos'), namespace='repuestos')),
-
-    # django-autocomplete-light
-    path('autocomplete/', include('dal.urls')),
-    path('autocomplete/', include('dal_select2.urls')),
+    # Se remueven rutas genéricas de DAL para evitar ModuleNotFoundError:
+    # path('autocomplete/', include('dal.urls')),
+    # path('autocomplete/', include('dal_select2.urls')),
 ]
 
 # Servir archivos multimedia en desarrollo
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
