52d5ca77606f972f48259086066dbacbfa7db352
AlessandroRG31 <danielalessandro1331@gmail.com>
2025-06-11 21:00:59 -0600

    Vistas completas y urls de vehículos y mantenimientos

--- mantenimiento/views.py
+++ mantenimiento/views.py
@@ -1,36 +1,111 @@
-from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView, UpdateView, DeleteView
-from .models import Vehiculo, Mantenimiento, RepuestoMantenimiento
-from .forms import VehiculoForm, MantenimientoForm
+from django.views.generic import (
+    ListView, DetailView,
+    CreateView, UpdateView, DeleteView
+)
+from .models import Vehiculo, Mantenimiento
+from .forms import VehiculoForm, MantenimientoForm, RepuestoMantenimientoFormSet
 
-# ... otras vistas ...
+class VehiculoListView(ListView):
+    model = Vehiculo
+    template_name = 'mantenimiento/vehiculo_list.html'
+    context_object_name = 'vehiculos'
+    paginate_by = 10
+    ordering = ['placa']
+
+class VehiculoDetailView(DetailView):
+    model = Vehiculo
+    template_name = 'mantenimiento/vehiculo_detail.html'
+    context_object_name = 'vehiculo'
 
 class VehiculoCreateView(CreateView):
     model = Vehiculo
     form_class = VehiculoForm
-    template_name = "mantenimiento/vehiculo_form.html"
+    template_name = 'mantenimiento/vehiculo_form.html'
     success_url = reverse_lazy('mantenimiento:vehiculo-list')
 
-    def get_form(self, form_class=None):
-        form = super().get_form(form_class)
+    def get_initial(self):
+        initial = super().get_initial()
         cliente_id = self.kwargs.get('cliente_id')
         if cliente_id:
-            form.fields['cliente'].queryset = form.fields['cliente'].queryset.filter(id=cliente_id)
-            form.fields['cliente'].initial = cliente_id
-            form.fields['cliente'].widget.attrs['readonly'] = True
-        return form
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['cliente_id'] = self.kwargs.get('cliente_id')
-        return context
+            initial['cliente'] = cliente_id
+        return initial
 
     def form_valid(self, form):
-        cliente_id = self.kwargs.get('cliente_id')
-        if cliente_id:
-            # Si llega cliente_id por la URL, lo asignamos manualmente
-            cliente = get_object_or_404(form.fields['cliente'].queryset.model, id=cliente_id)
-            form.instance.cliente = cliente
-        # Si no, deja que el formulario asigne el cliente por el campo select normal
+        self.object = form.save()
         return super().form_valid(form)
+
+class VehiculoUpdateView(UpdateView):
+    model = Vehiculo
+    form_class = VehiculoForm
+    template_name = 'mantenimiento/vehiculo_form.html'
+    success_url = reverse_lazy('mantenimiento:vehiculo-list')
+
+class VehiculoDeleteView(DeleteView):
+    model = Vehiculo
+    template_name = 'mantenimiento/vehiculo_confirm_delete.html'
+    success_url = reverse_lazy('mantenimiento:vehiculo-list')
+
+class MantenimientoListView(ListView):
+    model = Mantenimiento
+    template_name = 'mantenimiento/mantenimiento_list.html'
+    context_object_name = 'mantenimientos'
+    paginate_by = 10
+    ordering = ['-fecha_mantenimiento']
+
+class MantenimientoCreateView(CreateView):
+    model = Mantenimiento
+    form_class = MantenimientoForm
+    template_name = 'mantenimiento/mantenimiento_form.html'
+    success_url = reverse_lazy('mantenimiento:mantenimiento-list')
+
+    def get_initial(self):
+        initial = super().get_initial()
+        vehiculo_id = self.kwargs.get('vehiculo_id')
+        if vehiculo_id:
+            initial['vehiculo'] = vehiculo_id
+        return initial
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['repuesto_formset'] = RepuestoMantenimientoFormSet(self.request.POST or None)
+        return data
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context['repuesto_formset']
+        if formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return super().form_valid(form)
+        return self.render_to_response(self.get_context_data(form=form))
+
+class MantenimientoUpdateView(UpdateView):
+    model = Mantenimiento
+    form_class = MantenimientoForm
+    template_name = 'mantenimiento/mantenimiento_form.html'
+    success_url = reverse_lazy('mantenimiento:mantenimiento-list')
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['repuesto_formset'] = RepuestoMantenimientoFormSet(
+            self.request.POST or None,
+            instance=self.object
+        )
+        return data
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context['repuesto_formset']
+        if formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return super().form_valid(form)
+        return self.render_to_response(self.get_context_data(form=form))
+
+class MantenimientoDeleteView(DeleteView):
+    model = Mantenimiento
+    template_name = 'mantenimiento/mantenimiento_confirm_delete.html'
+    success_url = reverse_lazy('mantenimiento:mantenimiento-list')
