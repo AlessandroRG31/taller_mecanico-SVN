1edf318294e72764c1c57e94ae4eb0bdf454de20
AlessandroRG31 <danielalessandro1331@gmail.com>
2025-06-12 15:09:20 -0600

    Mejorar diseño del formulario de creación y edición de Empresa

--- repuestos/forms.py
+++ repuestos/forms.py
@@ -5,9 +5,9 @@ class EmpresaForm(forms.ModelForm):
     class Meta:
         model = Empresa
         fields = ['nombre', 'telefono']
-        labels = {
-            'nombre': 'Nombre de la empresa',
-            'telefono': 'Teléfono',
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'telefono': forms.TextInput(attrs={'class': 'form-control'}),
         }
 
 class RepuestoForm(forms.ModelForm):
@@ -20,7 +20,3 @@ class RepuestoForm(forms.ModelForm):
             'precio': forms.NumberInput(attrs={'class': 'form-control'}),
             'stock': forms.NumberInput(attrs={'class': 'form-control'}),
         }
-
-
-
-
