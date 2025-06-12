e186ee4f2e37e17fcd0281ac0233ef7144cd1159
AlessandroRG31 <danielalessandro1331@gmail.com>
2025-06-11 20:56:18 -0600

    Corrige asignación de cliente en la vista VehiculoCreateView y permite alta desde nuevo/<cliente_id>/

--- mantenimiento/views.py
+++ mantenimiento/views.py
@@ -1,83 +1,36 @@
-from django import forms
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
-from .models import Vehiculo, Mantenimiento
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from .models import Vehiculo, Mantenimiento, RepuestoMantenimiento
 from .forms import VehiculoForm, MantenimientoForm
 
-class VehiculoListView(ListView):
-    model = Vehiculo
-    template_name = 'mantenimiento/vehiculo_list.html'
-    context_object_name = 'vehiculos'
-
-class VehiculoDetailView(DetailView):
-    model = Vehiculo
-    template_name = 'mantenimiento/vehiculo_detail.html'
-    context_object_name = 'vehiculo'
+# ... otras vistas ...
 
 class VehiculoCreateView(CreateView):
     model = Vehiculo
     form_class = VehiculoForm
-    template_name = 'mantenimiento/vehiculo_form.html'
+    template_name = "mantenimiento/vehiculo_form.html"
     success_url = reverse_lazy('mantenimiento:vehiculo-list')
-
-    def get_initial(self):
-        initial = super().get_initial()
-        cliente_id = self.kwargs.get('cliente_id')
-        if cliente_id:
-            initial['cliente'] = cliente_id
-        return initial
 
     def get_form(self, form_class=None):
         form = super().get_form(form_class)
-        # Solo ocultar el campo si VIENE cliente_id en la URL
         cliente_id = self.kwargs.get('cliente_id')
         if cliente_id:
-            form.fields['cliente'].widget = forms.HiddenInput()
-        else:
-            form.fields['cliente'].widget = forms.Select()
+            form.fields['cliente'].queryset = form.fields['cliente'].queryset.filter(id=cliente_id)
+            form.fields['cliente'].initial = cliente_id
+            form.fields['cliente'].widget.attrs['readonly'] = True
         return form
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cliente_id'] = self.kwargs.get('cliente_id')
+        return context
+
     def form_valid(self, form):
         cliente_id = self.kwargs.get('cliente_id')
         if cliente_id:
-            form.instance.cliente_id = cliente_id
-        # Validar de nuevo antes de guardar
-        if not form.instance.cliente_id:
-            form.add_error('cliente', "Debes seleccionar un cliente.")
-            return self.form_invalid(form)
-        return super().form_valid(form)
-
-class VehiculoUpdateView(UpdateView):
-    model = Vehiculo
-    form_class = VehiculoForm
-    template_name = 'mantenimiento/vehiculo_form.html'
-    success_url = reverse_lazy('mantenimiento:vehiculo-list')
-
-class VehiculoDeleteView(DeleteView):
-    model = Vehiculo
-    template_name = 'mantenimiento/vehiculo_confirm_delete.html'
-    success_url = reverse_lazy('mantenimiento:vehiculo-list')
-
-class MantenimientoListView(ListView):
-    model = Mantenimiento
-    template_name = 'mantenimiento/mantenimiento_list.html'
-    context_object_name = 'mantenimientos'
-
-class MantenimientoCreateView(CreateView):
-    model = Mantenimiento
-    form_class = MantenimientoForm
-    template_name = 'mantenimiento/mantenimiento_form.html'
-    success_url = reverse_lazy('mantenimiento:mantenimiento-list')
-
-    def get_initial(self):
-        initial = super().get_initial()
-        vehiculo_id = self.kwargs.get('vehiculo_id')
-        if vehiculo_id:
-            initial['vehiculo'] = vehiculo_id
-        return initial
-
-    def form_valid(self, form):
-        vehiculo_id = self.kwargs.get('vehiculo_id')
-        if vehiculo_id:
-            form.instance.vehiculo_id = vehiculo_id
+            # Si llega cliente_id por la URL, lo asignamos manualmente
+            cliente = get_object_or_404(form.fields['cliente'].queryset.model, id=cliente_id)
+            form.instance.cliente = cliente
+        # Si no, deja que el formulario asigne el cliente por el campo select normal
         return super().form_valid(form)
